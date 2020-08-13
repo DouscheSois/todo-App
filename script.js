91adab90baa7cdbf3727d61fe3509058c551c718
Roger <roger.alex.aguilar@gmail.com>
2020-08-13 08:16:26 -0700

    updated js

--- script.js
+++ script.js
@@ -16,42 +16,67 @@ class TodoActions {
     todos.forEach((description) => TodoActions.addTodosToList(description));
   }
 
+  static createButton(buttonName, symbol) {
+    const btn = document.createElement("button");
+    btn.id = buttonName;
+    btn.classList.add(buttonName);
+    btn.textContent = symbol;
+    return btn;
+  }
+
+  static createTodo(todo) {
+    const newDiv = document.createElement("div");
+    const btnDiv = document.createElement("div");
+
+    btnDiv.classList.add("fr-buttons");
+
+    const todoText = document.createElement("h3");
+
+    todoText.textContent = todo.description;
+    todoText.classList.add("new-div-description");
+
+    const finishBtn = TodoActions.createButton("finishBtn", "✓");
+    const deleteBtn = TodoActions.createButton("deleteBtn", "X");
+
+    finishBtn.addEventListener("click", (e) =>
+      TodoActions.finishTodos(e.target)
+    );
+    deleteBtn.addEventListener("click", (e) =>
+      TodoActions.deleteTodos(e.target)
+    );
+
+    btnDiv.appendChild(finishBtn);
+    btnDiv.appendChild(deleteBtn);
+    newDiv.appendChild(todoText);
+    newDiv.appendChild(btnDiv);
+    return newDiv;
+  }
+
   static addTodosToList(todo) {
     const list = document.getElementById("input-list");
-    const newDiv = document.createElement("div");
-
-    newDiv.innerHTML = `
-    <h3 class="new-div-description" id="new-div-description">${
-      todo.description
-    }</h3>${" "}
-    <div class="fr-buttons">
-      <button class="finish-button" id="finish-button" type="button" name="button">✓</button>
-      <button class="remove-button" type="button" name="button">X</button>
-    </div>
-    `;
-
+    const newDiv = TodoActions.createTodo(todo);
     list.appendChild(newDiv);
+
+    // newDiv.innerHTML = `
+    // <h3 class="new-div-description" id="new-div-description">${
+    //   todo.description
+    // }</h3>${" "}
+    // <div class="fr-buttons">
+    //   <button class="finish-button" id="finish-button" type="button" name="button">✓</button>
+    //   <button class="remove-button" type="button" name="button">X</button>
+    // </div>
+    // `;
   }
 
   static deleteTodos(element) {
-    if (element.classList.contains("remove-button")) {
-      element.parentElement.parentElement.remove();
-    }
+    element.parentElement.parentElement.remove();
   }
 
   static finishTodos(element) {
-    if (element.classList.contains("finish-button")) {
-      if (
-        element.parentElement.parentElement.firstElementChild.style
-          .textDecoration !== "line-through"
-      ) {
-        element.parentElement.parentElement.firstElementChild.style.textDecoration =
-          "line-through";
-      } else {
-        element.parentElement.parentElement.firstElementChild.style.textDecoration =
-          "none";
-      }
-    }
+    const textElement = element.parentElement.parentElement.firstElementChild;
+    textElement.classList.contains("completed")
+      ? textElement.classList.remove("completed")
+      : textElement.classList.add("completed");
   }
 
   static clear() {
@@ -60,14 +85,6 @@ class TodoActions {
 }
 
 document.addEventListener("DOMContentLoaded", TodoActions.displayTodos);
-
-document.getElementById("input-list").addEventListener("click", (e) => {
-  TodoActions.deleteTodos(e.target);
-});
-
-document.getElementById("input-list").addEventListener("click", (e) => {
-  TodoActions.finishTodos(e.target);
-});
 
 document.getElementById("submit-button").addEventListener("click", (e) => {
   const key = randomID();
